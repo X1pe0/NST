7fb5cb3b49172860c365d231369c26efdf9e56b5
X1pe0 <9041120+X1pe0@users.noreply.github.com>
2021-12-29 21:02:19 -0500

    Add files via upload

--- NST/NST/My Project/Application.Designer.vb
+++ NST/NST/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.NS_T.Form1
+            Me.MainForm = Global.NS_T.loading
         End Sub
     End Class
 End Namespace
